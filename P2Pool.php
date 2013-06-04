ee3c9da38b88463d519ad28b43baeec7c58c3197
Ryan Null <ryan.null@gmail.com>
2013-06-03 23:23:37 -0400

    new p2pool plugin... it's rather simple with very few settings

---
+++ P2Pool.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Watches a p2pool server and drops alerts
+ *
+ * @category Phergie
+ * @package  Phergie_Plugin_P2Pool
+ * @author   Ryan Null <ryan@ryannull.com>
+ * @license  http://phergie.org/license New BSD License
+ * @link     http://pear.phergie.org/package/Phergie_Plugin_
+ */
+class Phergie_Plugin_P2Pool extends Phergie_Plugin_Abstract {
+	private $_nextBlockCheck = 0;
+	private $_nextSpeedCheck = 0;
+
+    /**
+     * Let's do some magic
+     *
+     * @return void
+     */
+    public function onTick() {
+		$time = time();
+
+		if ( $this->_nextBlockCheck <= $time ) {
+			$this->checkBlocks();
+			$this->_nextBlockCheck = time() + $this->getConfig('p2pool.blocks_interval');
+		}
+
+		if ( $this->_nextSpeedCheck <= $time ) {
+			$this->checkSpeed();
+			$this->_nextSpeedCheck = time() + $this->getConfig('p2pool.speed_interval');
+		}
+    }
+
+	public function onPrivMsg() {
+		switch ( $this->getEvent()->getArgument(1) ) {
+			case "!hashrate":
+				//set to zero and let the tick manager take care of it
+				$this->_nextSpeedCheck = 0;
+				break;
+		}
+	}
+
+	private function checkBlocks() {
+		$blocks = file_get_contents( $this->getConfig('p2pool.url') . "recent_blocks");
+
+		$blocks = json_decode($blocks);
+
+		if ( !file_exists( $this->getConfig('p2pool.storage') . "phergie-p2pool.dat" ) ) {
+			touch( $this->getConfig('p2pool.storage') . "phergie-p2pool.dat" );
+		}
+		$existingBlocks = explode("\n", file_get_contents( $this->getConfig('p2pool.storage') . "phergie-p2pool.dat") );
+
+		foreach ( $blocks as $block ) {
+			if ( !in_array($block->hash, $existingBlocks) ) {
+				$this->doPrivMsg($this->getConfig('p2pool.channel'), 
+					"NEW BLOCK [{$block->number}] has been found!");
+				$existingBlocks[] = $block->hash;
+			}
+		}
+
+		file_put_contents($this->getConfig('p2pool.storage') . "phergie-p2pool.dat", implode("\n", $existingBlocks) );
+	}
+
+	private function checkSpeed() {
+		$stats = file_get_contents( $this->getConfig('p2pool.url') . "global_stats");
+		$stats = json_decode($stats);
+
+		$localStats = file_get_contents( $this->getConfig('p2pool.url') . "local_stats");
+		$localStats = json_decode($localStats);
+
+		$this->doPrivMsg( $this->getConfig('p2pool.channel'),
+			"Pool Hash Rate: " . round($stats->pool_hash_rate / 1000, 2) . "kH/s");
+	}
+}
